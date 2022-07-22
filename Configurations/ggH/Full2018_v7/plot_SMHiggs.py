08a1046528a296d100fe4919007e921aa0425db7
Taylor Carnahan <tcarnaha@lxplus702.cern.ch>
2022-07-22 13:09:39 +0200

    plotter plugins for mkShapesMulti

--- Configurations/ggH/Full2018_v7/plot_SMHiggs.py
+++ Configurations/ggH/Full2018_v7/plot_SMHiggs.py
@@ -9,95 +9,95 @@
 #
 
 
-groupPlot['ggH_hww_125']  = {  
-                  'nameHR' : 'ggH_hww_125',
+groupPlot['ggH_hww_125_SMH']  = {  
+                  'nameHR' : 'ggH_hww_125_SMH',
                   'isSignal' : 0,
                   'color': 1,   # kBlack
-                  'samples'  : ['ggH_hww_125']
+                  'samples'  : ['ggH_hww_125_SMH']
               }
 
-groupPlot['ggH_hww_200']  = {  
-                  'nameHR' : 'ggH_hww_200',
+groupPlot['ggH_hww_200_SMH']  = {  
+                  'nameHR' : 'ggH_hww_200_SMH',
                   'isSignal' : 0,
                   'color': 603, # kBlue +3 
-                  'samples'  : ['ggH_hww_200']
+                  'samples'  : ['ggH_hww_200_SMH']
               }
 
-groupPlot['ggH_hww_300']  = {
-                  'nameHR' : 'ggH_hww_300',
+groupPlot['ggH_hww_300_SMH']  = {
+                  'nameHR' : 'ggH_hww_300_SMH',
                   'isSignal' : 0,
                   'color': 600,    # kBlue
-                  'samples'  : ['ggH_hww_300']
+                  'samples'  : ['ggH_hww_300_SMH']
 }
 
-groupPlot['ggH_hww_500']  = {  
-                  'nameHR' : "ggH_hww_500",
+groupPlot['ggH_hww_500_SMH']  = {  
+                  'nameHR' : "ggH_hww_500_SMH",
                   'isSignal' : 0,
                   'color': 857,    # kAzure -3
-                  'samples'  : ['ggH_hww_500']
+                  'samples'  : ['ggH_hww_500_SMH']
               }
 
-groupPlot['ggH_hww_600']  = {
-                  'nameHR' : 'ggH_hww_600',
+groupPlot['ggH_hww_600_SMH']  = {
+                  'nameHR' : 'ggH_hww_600_SMH',
                   'isSignal' : 0,
                   'color': 430, # kCyan -2                                                        
-                  'samples'  : ['ggH_hww_600']
+                  'samples'  : ['ggH_hww_600_SMH']
               }
 
-groupPlot['ggH_hww_700']  = {  
-                  'nameHR' : 'ggH_hww_700',
+groupPlot['ggH_hww_700_SMH']  = {  
+                  'nameHR' : 'ggH_hww_700_SMH',
                   'isSignal' : 0,
                   'color': 413, # kGreen -3  
-                  'samples'  : ['ggH_hww_700']
+                  'samples'  : ['ggH_hww_700_SMH']
               }
 
-groupPlot['ggH_hww_800']  = {
-                  'nameHR' : 'ggH_hww_800',
+groupPlot['ggH_hww_800_SMH']  = {
+                  'nameHR' : 'ggH_hww_800_SMH',
                   'isSignal' : 0,
                   'color': 797, # kOrange -3                                                      
-                  'samples'  : ['ggH_hww_800']
+                  'samples'  : ['ggH_hww_800_SMH']
               }
 
-groupPlot['ggH_hww_900']  = {
-                  'nameHR' : 'ggH_hww_900',
+groupPlot['ggH_hww_900_SMH']  = {
+                  'nameHR' : 'ggH_hww_900_SMH',
                   'isSignal' : 0,
                   'color': 634,    # kRed +2                                                     
-                  'samples'  : ['ggH_hww_900']
+                  'samples'  : ['ggH_hww_900_SMH']
 }
 
-groupPlot['ggH_hww_1000']  = {
-                  'nameHR' : "ggH_hww_1000",
+groupPlot['ggH_hww_1000_SMH']  = {
+                  'nameHR' : "ggH_hww_1000_SMH",
                   'isSignal' : 0,
                   'color': 628,    # kRed-4                                                      
-                  'samples'  : ['ggH_hww_1000']
+                  'samples'  : ['ggH_hww_1000_SMH']
               }
 
-# groupPlot['ggH_hww_1500']  = {
-#                   'nameHR' : 'ggH_hww_1500',
+# groupPlot['ggH_hww_1500_SMH']  = {
+#                   'nameHR' : 'ggH_hww_1500_SMH',
 #                   'isSignal' : 0,
 #                   'color': 618, # kMagenta +3                                                     
-#                   'samples'  : ['ggH_hww_1500']
+#                   'samples'  : ['ggH_hww_1500_SMH']
 #               }
 
-# groupPlot['ggH_hww_2000']  = {
-#                   'nameHR' : 'ggH_hww_2000',
+# groupPlot['ggH_hww_2000_SMH']  = {
+#                   'nameHR' : 'ggH_hww_2000_SMH',
 #                   'isSignal' : 0,
 #                   'color': 893, # kPink -7                                                       
-#                   'samples'  : ['ggH_hww_2000']
+#                   'samples'  : ['ggH_hww_2000_SMH']
 #               }
 
-# groupPlot['ggH_hww_2500']  = {
-#                   'nameHR' : "ggH_hww_2500",
+# groupPlot['ggH_hww_2500_SMH']  = {
+#                   'nameHR' : "ggH_hww_2500_SMH",
 #                   'isSignal' : 0,
 #                   'color': 883,    # kViolet+3                                                     
-#                   'samples'  : ['ggH_hww_2500']
+#                   'samples'  : ['ggH_hww_2500_SMH']
 #               }
 
-# groupPlot['ggH_hww_3000']  = {
-#                   'nameHR' : 'ggH_hww_3000',
+# groupPlot['ggH_hww_3000_SMH']  = {
+#                   'nameHR' : 'ggH_hww_3000_SMH',
 #                   'isSignal' : 0,
 #                   'color': 923, # kGray +3                                                        
-#                   'samples'  : ['ggH_hww_3000']
+#                   'samples'  : ['ggH_hww_3000_SMH']
 #               }
 
 
@@ -107,99 +107,98 @@ groupPlot['ggH_hww_1000']  = {
 # keys here must match keys in samples.py    
 #                    
 
-plot['ggH_hww_125'] = {
-                  'nameHR' : 'ggH_hww_125',
+plot['ggH_hww_125_SMH'] = {
                   'color': 1,                                                                                          
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_200']  = {
+plot['ggH_hww_200_SMH']  = {
                   'color': 603,                                                                                          
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0   # ele/mu trigger efficiency   datadriven                                                         
                   }
 
-plot['ggH_hww_300']  = {
+plot['ggH_hww_300_SMH']  = {
                   'color': 600,                                                                                       
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0
               }
 
-plot['ggH_hww_500']  = {
+plot['ggH_hww_500_SMH']  = {
                   'color': 857,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_600']  = {
+plot['ggH_hww_600_SMH']  = {
                   'color': 430,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_700']  = {
+plot['ggH_hww_700_SMH']  = {
                   'color': 413,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_800']  = {
+plot['ggH_hww_800_SMH']  = {
                   'color': 797,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_900']  = {
+plot['ggH_hww_900_SMH']  = {
                   'color': 634,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_1000']  = {
+plot['ggH_hww_1000_SMH']  = {
                   'color': 628,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-# plot['ggH_hww_1500']  = {
+# plot['ggH_hww_1500_SMH']  = {
 #                   'color': 618,                                                                                        
 #                   'isSignal' : 0,
 #                   'isData'   : 0,
 #                   'scale'    : 1.0,
 #                   }
 
-# plot['ggH_hww_2000']  = {
+# plot['ggH_hww_2000_SMH']  = {
 #                   'color': 857,                                                                                          
 #                   'isSignal' : 0,
 #                   'isData'   : 0,                                                                                                    
 #                   'scale'    : 1.0
 #                   }
 
-# plot['ggH_hww_2500']  = {
+# plot['ggH_hww_2500_SMH']  = {
 #                   'color': 883,                                                                                        
 #                   'isSignal' : 0,
 #                   'isData'   : 0,
 #                   'scale'    : 1.0,
 #                   }
 
-# plot['ggH_hww_3000']  = {
+# plot['ggH_hww_3000_SMH']  = {
 #                   'color': 923,                                                                                        
 #                   'isSignal' : 0,
 #                   'isData'   : 0,
 #                   'scale'    : 1.0,
 #                   }
 
-# data
+# Data
 
 # plot['ZH_htt']  = { 
 #                   'nameHR' : 'Data',
--- Configurations/ggH/Full2018_v7/plot_SMHiggsProp.py
+++ Configurations/ggH/Full2018_v7/plot_SMHiggsProp.py
@@ -9,95 +9,95 @@
 #
 
 
-groupPlot['ggH_hww_125']  = {  
-                  'nameHR' : 'ggH_hww_125',
+groupPlot['ggH_hww_125_SMHProp']  = {  
+                  'nameHR' : 'ggH_hww_125_SMHProp',
                   'isSignal' : 0,
                   'color': 1,   # kBlack
-                  'samples'  : ['ggH_hww_125']
+                  'samples'  : ['ggH_hww_125_SMHProp']
               }
 
-groupPlot['ggH_hww_200']  = {  
-                  'nameHR' : 'ggH_hww_200',
+groupPlot['ggH_hww_200_SMHProp']  = {  
+                  'nameHR' : 'ggH_hww_200_SMHProp',
                   'isSignal' : 0,
                   'color': 603, # kBlue +3 
-                  'samples'  : ['ggH_hww_200']
+                  'samples'  : ['ggH_hww_200_SMHProp']
               }
 
-groupPlot['ggH_hww_300']  = {
-                  'nameHR' : 'ggH_hww_300',
+groupPlot['ggH_hww_300_SMHProp']  = {
+                  'nameHR' : 'ggH_hww_300_SMHProp',
                   'isSignal' : 0,
                   'color': 600,    # kBlue
-                  'samples'  : ['ggH_hww_300']
+                  'samples'  : ['ggH_hww_300_SMHProp']
 }
 
-groupPlot['ggH_hww_500']  = {  
-                  'nameHR' : "ggH_hww_500",
+groupPlot['ggH_hww_500_SMHProp']  = {  
+                  'nameHR' : "ggH_hww_500_SMHProp",
                   'isSignal' : 0,
                   'color': 857,    # kAzure -3
-                  'samples'  : ['ggH_hww_500']
+                  'samples'  : ['ggH_hww_500_SMHProp']
               }
 
-groupPlot['ggH_hww_600']  = {
-                  'nameHR' : 'ggH_hww_600',
+groupPlot['ggH_hww_600_SMHProp']  = {
+                  'nameHR' : 'ggH_hww_600_SMHProp',
                   'isSignal' : 0,
                   'color': 430, # kCyan -2                                                        
-                  'samples'  : ['ggH_hww_600']
+                  'samples'  : ['ggH_hww_600_SMHProp']
               }
 
-groupPlot['ggH_hww_700']  = {  
-                  'nameHR' : 'ggH_hww_700',
+groupPlot['ggH_hww_700_SMHProp']  = {  
+                  'nameHR' : 'ggH_hww_700_SMHProp',
                   'isSignal' : 0,
                   'color': 413, # kGreen -3  
-                  'samples'  : ['ggH_hww_700']
+                  'samples'  : ['ggH_hww_700_SMHProp']
               }
 
-groupPlot['ggH_hww_800']  = {
-                  'nameHR' : 'ggH_hww_800',
+groupPlot['ggH_hww_800_SMHProp']  = {
+                  'nameHR' : 'ggH_hww_800_SMHProp',
                   'isSignal' : 0,
                   'color': 797, # kOrange -3                                                      
-                  'samples'  : ['ggH_hww_800']
+                  'samples'  : ['ggH_hww_800_SMHProp']
               }
 
-groupPlot['ggH_hww_900']  = {
-                  'nameHR' : 'ggH_hww_900',
+groupPlot['ggH_hww_900_SMHProp']  = {
+                  'nameHR' : 'ggH_hww_900_SMHProp',
                   'isSignal' : 0,
                   'color': 634,    # kRed +2                                                     
-                  'samples'  : ['ggH_hww_900']
+                  'samples'  : ['ggH_hww_900_SMHProp']
 }
 
-groupPlot['ggH_hww_1000']  = {
-                  'nameHR' : "ggH_hww_1000",
+groupPlot['ggH_hww_1000_SMHProp']  = {
+                  'nameHR' : "ggH_hww_1000_SMHProp",
                   'isSignal' : 0,
                   'color': 628,    # kRed-4                                                      
-                  'samples'  : ['ggH_hww_1000']
+                  'samples'  : ['ggH_hww_1000_SMHProp']
               }
 
-# groupPlot['ggH_hww_1500']  = {
-#                   'nameHR' : 'ggH_hww_1500',
+# groupPlot['ggH_hww_1500_SMHProp']  = {
+#                   'nameHR' : 'ggH_hww_1500_SMHProp',
 #                   'isSignal' : 0,
 #                   'color': 618, # kMagenta +3                                                     
-#                   'samples'  : ['ggH_hww_1500']
+#                   'samples'  : ['ggH_hww_1500_SMHProp']
 #               }
 
-# groupPlot['ggH_hww_2000']  = {
-#                   'nameHR' : 'ggH_hww_2000',
+# groupPlot['ggH_hww_2000_SMHProp']  = {
+#                   'nameHR' : 'ggH_hww_2000_SMHProp',
 #                   'isSignal' : 0,
 #                   'color': 893, # kPink -7                                                       
-#                   'samples'  : ['ggH_hww_2000']
+#                   'samples'  : ['ggH_hww_2000_SMHProp']
 #               }
 
-# groupPlot['ggH_hww_2500']  = {
-#                   'nameHR' : "ggH_hww_2500",
+# groupPlot['ggH_hww_2500_SMHProp']  = {
+#                   'nameHR' : "ggH_hww_2500_SMHProp",
 #                   'isSignal' : 0,
 #                   'color': 883,    # kViolet+3                                                     
-#                   'samples'  : ['ggH_hww_2500']
+#                   'samples'  : ['ggH_hww_2500_SMHProp']
 #               }
 
-# groupPlot['ggH_hww_3000']  = {
-#                   'nameHR' : 'ggH_hww_3000',
+# groupPlot['ggH_hww_3000_SMHProp']  = {
+#                   'nameHR' : 'ggH_hww_3000_SMHProp',
 #                   'isSignal' : 0,
 #                   'color': 923, # kGray +3                                                        
-#                   'samples'  : ['ggH_hww_3000']
+#                   'samples'  : ['ggH_hww_3000_SMHProp']
 #               }
 
 
@@ -107,92 +107,91 @@ groupPlot['ggH_hww_1000']  = {
 # keys here must match keys in samples.py    
 #                    
 
-plot['ggH_hww_125'] = {
-                  'nameHR' : 'ggH_hww_125',
+plot['ggH_hww_125_SMHProp'] = {
                   'color': 1,                                                                                          
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_200']  = {
+plot['ggH_hww_200_SMHProp']  = {
                   'color': 603,                                                                                          
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0   # ele/mu trigger efficiency   datadriven                                                         
                   }
 
-plot['ggH_hww_300']  = {
+plot['ggH_hww_300_SMHProp']  = {
                   'color': 600,                                                                                       
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0
               }
 
-plot['ggH_hww_500']  = {
+plot['ggH_hww_500_SMHProp']  = {
                   'color': 857,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_600']  = {
+plot['ggH_hww_600_SMHProp']  = {
                   'color': 430,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_700']  = {
+plot['ggH_hww_700_SMHProp']  = {
                   'color': 413,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_800']  = {
+plot['ggH_hww_800_SMHProp']  = {
                   'color': 797,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_900']  = {
+plot['ggH_hww_900_SMHProp']  = {
                   'color': 634,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-plot['ggH_hww_1000']  = {
+plot['ggH_hww_1000_SMHProp']  = {
                   'color': 628,                                                                                        
                   'isSignal' : 0,
                   'isData'   : 0,
                   'scale'    : 1.0,
                   }
 
-# plot['ggH_hww_1500']  = {
+# plot['ggH_hww_1500_SMHProp']  = {
 #                   'color': 618,                                                                                        
 #                   'isSignal' : 0,
 #                   'isData'   : 0,
 #                   'scale'    : 1.0,
 #                   }
 
-# plot['ggH_hww_2000']  = {
+# plot['ggH_hww_2000_SMHProp']  = {
 #                   'color': 857,                                                                                          
 #                   'isSignal' : 0,
 #                   'isData'   : 0,                                                                                                    
 #                   'scale'    : 1.0
 #                   }
 
-# plot['ggH_hww_2500']  = {
+# plot['ggH_hww_2500_SMHProp']  = {
 #                   'color': 883,                                                                                        
 #                   'isSignal' : 0,
 #                   'isData'   : 0,
 #                   'scale'    : 1.0,
 #                   }
 
-# plot['ggH_hww_3000']  = {
+# plot['ggH_hww_3000_SMHProp']  = {
 #                   'color': 923,                                                                                        
 #                   'isSignal' : 0,
 #                   'isData'   : 0,
