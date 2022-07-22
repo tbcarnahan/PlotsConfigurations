36807a7845e634e174e43ae559cb90a01ecb1565
Taylor Carnahan <tcarnaha@lxplus733.cern.ch>
2022-07-22 15:07:00 +0200

    all the updates for HWW ggH

--- Configurations/ggH/Full2018_v7/cuts.py
+++ Configurations/ggH/Full2018_v7/cuts.py
@@ -1,86 +1,18 @@
 # cuts
 
-supercut = ' mll > 12 \
-            && Lepton_pt[0]>25 \
-            && Lepton_pt[1]>13 \
-            && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) \
-            && abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5 \
-            && ptll>30 \
-            && PuppiMET_pt > 20 \
-            && (Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13) \
-            '
+_tmp = [ 
+     'mll>0.',
+       ]
 
-## Signal regions
-cuts['hww2l2v_13TeV'] = {
-   'expr': 'sr',
-    # Define the sub-categorization of sr
-   'categories' : {
-      'pm_0j_pt2ge20' : ' Lepton_pdgId[0] < 0 && Lepton_pt[1]>=20 && zeroJet',
-      'mp_0j_pt2ge20' : ' Lepton_pdgId[0] > 0 && Lepton_pt[1]>=20 && zeroJet',
-      #
-      'pm_0j_pt2lt20' : ' Lepton_pdgId[0] < 0 && Lepton_pt[1]<20 && zeroJet',
-      'mp_0j_pt2lt20' : ' Lepton_pdgId[0] > 0 && Lepton_pt[1]<20 && zeroJet',
-      #
-      'pm_1j_pt2ge20' : ' Lepton_pdgId[0] < 0 && Lepton_pt[1]>=20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
-      'mp_1j_pt2ge20' : ' Lepton_pdgId[0] > 0 && Lepton_pt[1]>=20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
-      #
-      'pm_1j_pt2lt20' : ' Lepton_pdgId[0] < 0 && Lepton_pt[1]<20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
-      'mp_1j_pt2lt20' : ' Lepton_pdgId[0] > 0 && Lepton_pt[1]<20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
-      # FIXME fix the mjj or additional cuts to make this orthogonal to VH2j and VBF
-      '2j'               : ' (mjj<65 || mjj>105) && mjj<120 && multiJet', 
-   }
-}
+supercut = ' && '.join(_tmp)
 
-## Top control regions
-cuts['hww2l2v_13TeV_top']  = { 
-   'expr' : 'topcr',
-    # Define the sub-categorization of topcr
-   'categories' : {
-      '0j' : 'zeroJet',
-      '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
-      '2j' : '(mjj<65 || mjj>105) && mjj<120 && multiJet',
-   }
-}
-'''
-cuts['hww2l2v_13TeV_top_ee']  = {
-   'expr' : 'topcr',
-    # Define the sub-categorization of topcr
-   'categories' : {
-      '0j' : 'zeroJet && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*11',
-      '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30 && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*11',
-      '2j' : '(mjj<65 || mjj>105) && mjj<120 && multiJet && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*11',
-   }
-}
+def addcut(name, exprs):
+    cuts[name] = ' && '.join(exprs)
 
-cuts['hww2l2v_13TeV_top_ee']  = {
-   'expr' : 'topcr',
-    # Define the sub-categorization of topcr
-   'categories' : {
-      '0j' : 'zeroJet && Lepton_pdgId[0]*Lepton_pdgId[1] == -13*13',
-      '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30 && Lepton_pdgId[0]*Lepton_pdgId[1] == -13*13',
-      '2j' : '(mjj<65 || mjj>105) && mjj<120 && multiJet && Lepton_pdgId[0]*Lepton_pdgId[1] == -13*13',
-   }
-}
-'''
-## DYtt control regions
-cuts['hww2l2v_13TeV_dytt']  = { 
-   'expr' : 'dycr',
-   # Define the sub-categorization of dycr
-   'categories' : { 
-      '0j' : 'zeroJet',
-      '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
-      '2j' : '(mjj<65 || mjj>105) && mjj<120 && multiJet',
-   }
-}
-'''
-## WW control regions
-## Used only for control plots, no need to add these cuts for the fit
-cuts['hww2l2v_13TeV_WW'] = {
- 'expr' : 'wwcr',
- 'categories' : {
-   '0j' : 'zeroJet',
-   '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
-   '2j' : '(mjj<65 || mjj>105) && mjj<120 && multiJet'
- }
-}
-'''
+
+_tmp = [
+    'Lepton_pt[0] > 0.',
+       ]
+
+addcut('test', _tmp)
+
